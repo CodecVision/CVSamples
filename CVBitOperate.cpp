f1ba4cca4d765bfb9e61da07b8968db43eb5f12f
codecvision <icodecvision@gmail.com>
2022-04-14 17:12:14 +0800

    Add cvbit operate

---
+++ CVBitOperate.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Leo_Liu on 2022/4/14.
+//
+
+#include "CVBitOperate.h"
---
+++ CVBitOperate.h
@@ -0,0 +1,14 @@
+//
+// Created by Leo_Liu on 2022/4/14.
+//
+
+#ifndef CVSAMPLES_CVBITOPERATE_H
+#define CVSAMPLES_CVBITOPERATE_H
+
+
+class CVBitOperate {
+
+};
+
+
+#endif //CVSAMPLES_CVBITOPERATE_H
